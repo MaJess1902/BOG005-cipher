8febb909f40e8e712637e34805c19f7a762d646a
Mariana <mjsc1902@gmail.com>
2022-07-25 08:22:33 -0500

    funcion cipher logra cifrar de manera correcta, agregué todos los estilos

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,14 @@
 const cipher = {
-  // ...
+  encode: function cifrar(mensaje, desplazamiento) {
+    let resultado = "";
+    for (var i = 0; i < mensaje.length; i++) {
+      let mensajeCifrado = ((mensaje.charCodeAt(i) + desplazamiento - 65) % 26) + 65;
+      resultado += String.fromCharCode(mensajeCifrado);
+    }
+    document.getElementById("inputCifrado-1").innerHTML = resultado;
+    return resultado;
+  },
 };
 
-export default cipher;
+export default cipher; /*archivo original */
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import cipher from "./cipher.js";
 
-let botonCifrar = document.getElementById('cifrar-btn');
-botonCifrar.addEventListener("click", cifrar);
+let botonCifrar = document.getElementById("cifrar-btn");
+let botonDecifrar = document.getElementById("cifrar-btn");
 
-function cifrar() {
-  let mensaje = document.getElementById("textoACifrar").value.toUpperCase();
-//   this.value = this.value.toUpperCase();
-  console.log(mensaje);
-}
-
-
+botonCifrar.addEventListener(
+  "click",
+  function () {
+    let mensaje = document.getElementById("textoACifrar").value.toUpperCase();
+    let desplazamiento = document.getElementById("desplazamiento").value;
+    document.getElementById("inputCifrado-1").innerHTML = cipher.encode(mensaje, parseInt(desplazamiento));
+  },
+  false
+);
 
