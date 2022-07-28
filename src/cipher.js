85d08101c24af1cbfc03e37689697d4f07e1c8d6
Mariana <mjsc1902@gmail.com>
2022-07-28 11:39:29 -0500

    termino de la funcion decifrar, cambio parcial en los estilos del index,y se agregó una verificacion de datos al momento de cifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,26 @@
 const cipher = {
-  encode: function cifrar(mensaje, desplazamiento) {
+  encode: function cifrar(offset, mensaje) {
+    if (offset == null || mensaje == [] || offset === 0) {
+      throw new TypeError("Debes darme un número de desplazamiento y un mensaje");
+    }
     let resultado = "";
     for (var i = 0; i < mensaje.length; i++) {
-      let mensajeCifrado = ((mensaje.charCodeAt(i) + desplazamiento - 65) % 26) + 65;
+      let mensajeCifrado = ((mensaje.charCodeAt(i) + offset - 65) % 26) + 65;
       resultado += String.fromCharCode(mensajeCifrado);
     }
-    document.getElementById("inputCifrado-1").innerHTML = resultado;
+    return resultado;
+  },
+  decode: function decifrar(offset, mensaje) {
+    if (offset == null || mensaje == [] || offset === 0) {
+      throw new TypeError("Debes darme un número de desplazamiento y un mensaje");
+    }
+    let resultado = "";
+    for (var i = 0; i < mensaje.length; i++) {
+      let mensajeCifrado = ((mensaje.charCodeAt(i) + 65 - offset) % 26) + 65;
+      resultado += String.fromCharCode(mensajeCifrado);
+    }
     return resultado;
   },
 };
 
 export default cipher; /*archivo original */
-
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import cipher from "./cipher.js";
 
 let botonCifrar = document.getElementById("cifrar-btn");
-let botonDecifrar = document.getElementById("cifrar-btn");
+let botonDecifrar = document.getElementById("decifrar-btn");
 
 botonCifrar.addEventListener(
   "click",
   function () {
     let mensaje = document.getElementById("textoACifrar").value.toUpperCase();
-    let desplazamiento = document.getElementById("desplazamiento").value;
-    document.getElementById("inputCifrado-1").innerHTML = cipher.encode(mensaje, parseInt(desplazamiento));
-  },
-  false
+    let offset = document.getElementById("desplazamiento").value;
+    document.getElementById("inputCifrado-1").value = cipher.encode(parseInt(offset),mensaje);
+  },false
+);
+
+botonDecifrar.addEventListener(
+  "click", 
+  function(){
+    let mensaje = document.getElementById("textoACifrar").value.toUpperCase();
+    let offset = document.getElementById("desplazamiento").value;
+    document.getElementById("inputCifrado-1").value = cipher.decode(parseInt(offset),mensaje);
+  },false
 );
 
